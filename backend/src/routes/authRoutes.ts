6100de1e36be884e0948445b3bc975737a6358f0
yuki262 <furkazar@gmail.com>
2025-07-16 22:26:52 +0100

    chore: formatting

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -4,9 +4,7 @@ import { login, logout, signup } from "../controllers/authController";
 const authRoutes = express.Router();
 
 authRoutes.get("/signup", signup);
-
 authRoutes.get("/login", login);
-
 authRoutes.get("/logout", logout);
 
 export { authRoutes };
