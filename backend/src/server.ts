ebe711a59e8ac3550d050f7dcbf97c976fcd192f
yuki262 <furkazar@gmail.com>
2025-07-16 22:31:43 +0100

    chore: adjust server message

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
